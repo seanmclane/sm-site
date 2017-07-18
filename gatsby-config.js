e608b6592589829c270e83d8e0a6ff3c38991cd2
Sean McLane <smclane@Seans-MacBook-Pro-2.local>
2017-07-17 20:29:33 -0600

    updated post folder and image processing config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 740,
+              quality: 90
             },
           },
           `gatsby-remark-prismjs`,
